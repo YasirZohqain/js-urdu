2ff99a65df6fb71c54485c48bc2b96180ade3890
Yasir Mehmood <yasirzohqain@gmail.com>
2025-02-02 12:58:53 +0000

    Stack Memory and Heap Memory

--- 01_basic/05_dataTypeSummary.js
+++ 01_basic/05_dataTypeSummary.js
@@ -20,3 +20,38 @@ Return type of variables in JavaScript
 
 
 // Interview::::: JavaScript is a dynamically typed language. 
+
+
+
+// *********************** Stack Memory and Heap Memory ***************************************** //
+
+
+let assignEmail = 'Yasir@hotmail.com';
+
+let superEmail = assignEmail;
+
+
+
+superEmail = 'hero@hotmail.com'
+
+//console.log(superEmail)
+
+//console.log(assignEmail)
+
+
+
+
+let userOne = {
+
+       email: 'yasirzohqain@gmail.com',
+       name: 'yasir'
+}
+
+
+let userTwo = userOne
+
+userTwo.email = "yasir_zohqain@hotmail.com"
+
+
+console.log(userTwo.email)
+console.log(userOne.email)
