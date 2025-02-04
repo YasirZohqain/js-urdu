ebcede7f35bd091f9de7c3b07df431b4247ba549
Yasir Mehmood <yasirzohqain@gmail.com>
2025-02-04 13:52:57 +0000

    Number

---
+++ 01_basic/06_number.js
@@ -0,0 +1,12 @@
+// console.log(Math.random())
+
+
+// console.log(Math.floor(Math.random() * 6) +1)
+
+
+const min = 10;
+
+const max = 20;
+
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
