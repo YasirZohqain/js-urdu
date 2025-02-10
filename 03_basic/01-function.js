3ecfbdf1e079feb54d30d2ce9aceaaaf5f15c9f9
Yasir Mehmood <yasirzohqain@gmail.com>
2025-02-10 10:07:49 +0000

    functionTopicbasic

--- 03_basic/01-function.js
+++ 03_basic/01-function.js
@@ -13,27 +13,60 @@ function callMyName () {
 // the function excution is "()" 
 
 
-// function xyz (num1, num2) {
-//     let totalnum = num1 + num2
-//     return totalnum;
-// }
-
 function xyz (num1, num2) {
-    return num1 + num2
+    const total = num1 + num2
+    return total;
 }
+
+// function xyz (num1, num2) {
+//     return num1 + num2
+// }
 
 const total = xyz(2,5);
 
 console.log(total)
 
 function loginMassage(userName = 'zohqain') {
-    if (userName === undefined) {
+    if (userName === undefined) /* (!userName)*/ {
         console.log('Invaild user')
         return
     }
    return  `${userName} is login`
 }
 
+
 const msg = loginMassage()
 
 console.log(msg)
+
+
+
+//...items when use DOT DOT DOT then name is call rest oprator
+function addToCart(...items) {
+    return items
+}
+
+
+const cartData = addToCart(100,500,600,2000);
+
+console.log(cartData)
+
+
+const objValue = {
+    naam: 'Yasir',
+    phone: '03002831911'
+}
+
+function anyObj (getObj) {
+    return getObj
+}
+
+console.log(anyObj(objValue.naam))
+
+const arrvalue = [200,500,600,200,900]
+
+function anyArr (getArr) {
+    return getArr
+}
+
+console.log(anyArr(arrvalue))
